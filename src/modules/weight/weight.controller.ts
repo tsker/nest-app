585f31c2c7e1ab31f25da23a22e03b32d9dc39a8
luofei <477800969@qq.com>
2017-11-16 11:34:11 +0800

    default title optimize

--- src/modules/weight/weight.controller.ts
+++ src/modules/weight/weight.controller.ts
@@ -1,37 +1,29 @@
 import { HttpException } from '@nestjs/core';
-import {
-    Controller,
-    Get,
-    Post,
-    HttpStatus,
-    Res,
-    Req,
-    Body,
-    UsePipes
-} from '@nestjs/common';
+import { Controller, Get, Post, HttpStatus, Res, Req, Body, UsePipes } from '@nestjs/common';
 import * as moment from 'moment';
 
 import { ValidationPipe } from '../../middles/validation.pipe';
 import { WeightService, WeightModel } from './weight.service';
 
 const li = function(item) {
-    return `<li>${JSON.stringify(item)}</li>`;
+	return `<li>${JSON.stringify(item)}</li>`;
 };
 @Controller('weight')
 export class WeightController {
-    constructor(private weightService: WeightService) {}
+	constructor(private weightService: WeightService) {}
 
-    @Get()
-    async index(@Res() res) {
-        let list = await this.weightService.get();
-        res.render('weight', { list });
-    }
+	@Get()
+	async index(@Res() res) {
+		let list = await this.weightService.get();
+		res.render('weight', { list });
+	}
 
-    @Post()
-    post(@Body() body) {
-        return this.weightService.add({
-            ...body,
-            date: moment().format('YYYY-MM-DD HH:mm:ss')
-        });
-    }
+	@Post()
+	post(
+		@Body('', new ValidationPipe())
+		body: WeightModel
+	) {
+		let currentDate = moment().format('YYYY-MM-DD HH:mm:ss');
+		return this.weightService.add({ ...body, date: currentDate });
+	}
 }
--- src/modules/weight/weight.service.ts
+++ src/modules/weight/weight.service.ts
@@ -1,8 +1,11 @@
 import { Component } from '@nestjs/common';
+import { IsNumberString} from "class-validator";
 import * as fs from 'fs';
 
-export interface WeightModel {
-	weight: number;
+export class WeightModel {
+	@IsNumberString()
+	value: number;
+
 	date: string;
 }
 
@@ -12,17 +15,15 @@ export class WeightService {
 	private list: WeightModel[] = [];
 
 	add(w: WeightModel) {
-		return new Promise((ok) => {
-			let data = this.get();
-			data.push(w);
-			fs.writeFile(this.filename, JSON.stringify(data, null, 4), 'utf-8', function(){
-				ok(w)
-			});
-		});
+		let data = this.get();
+		data.push(w);
+
+		fs.writeFileSync(this.filename, JSON.stringify(data, null, 4), 'utf-8');
+		return w;
 	}
 
 	get(): any[] {
-		let data = fs.readFileSync(this.filename).toString();
+		let data = fs.readFileSync(this.filename).toString().trim();
 		return data.length ? JSON.parse(data) : [];
 	}
 }
