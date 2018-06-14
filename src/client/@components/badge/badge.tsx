3a7d1f874ee23fa4cf7b2296422f8a8cb6727f82
luofei <477800969@qq.com>
2018-06-14 16:55:09 +0800

    optimize badge

--- src/client/@components/badge/badge.tsx
+++ src/client/@components/badge/badge.tsx
@@ -4,9 +4,13 @@ import * as cls from 'classnames';
 import './badge.less';
 
 interface BadgeProps extends HtmlHTMLAttributes<HTMLSpanElement> {
-	skin?: string;
+    skin?: string;
+    dot?: boolean;
 }
 
-export function Badge({ className, skin = 'primary', ...props }: BadgeProps) {
-	return <span className={cls('badge', `skin-${skin}`, className)} {...props} />;
+export function Badge ({ className, skin = 'primary', dot, ...props }: BadgeProps) {
+
+    const classname = cls(dot ? 'badge-dot' : 'badge', `skin-${skin}`, className);
+    return <span className={classname} {...props} />;
+
 }
